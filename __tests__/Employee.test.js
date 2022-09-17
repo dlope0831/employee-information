a5df1028fa92e726f0826b994c350b3d1427ad70
Daniela Lopez <dlope0831@gmail.com>
2022-09-17 10:07:31 -0500

    added code to intern, employee, manager js file

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,8 @@
+const Employee = require('.../Employee');
+const Engineer = requre('.../Engineer');
+const Intern = require('.../Intern');
+const Manager = require('.../Manager');
+
+test('creates a new employee object', () => {
+
+})
